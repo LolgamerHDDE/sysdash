f611a860e79ffd37ca1ac370604aa3b77c170bba
LolgamerHDDE <dahlemlinus@gmail.com>
2025-07-04 22:29:19 +0200

    Added first Sites

--- main.py
+++ main.py
@@ -3,18 +3,47 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
+# Exernal URLS
+
+BOOTSTRAP_CSS_1 = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
+BOOTSTRAP_JS_1 = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+
+# Defining App and Static Directorys
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 template = Jinja2Templates(directory="frontend")
 
+# Root Directory
 @app.get("/")
 async def root(requests: Request):
-    return {
-        "message": "Hello World!",
-        "requests": requests
-    }
+    return template.TemplateResponse("index.html",
+                                     {
+                                         "request": requests,
+                                         "BOOTSTRAP_CSS_1": BOOTSTRAP_CSS_1,
+                                         "BOOTSTRAP_JS_1": BOOTSTRAP_JS_1
+                                     })
 
+# Components List
+@app.get("/components")
+async def components(requests: Request):
+        return template.TemplateResponse("components.html",
+                                     {
+                                         "request": requests,
+                                         "BOOTSTRAP_CSS_1": BOOTSTRAP_CSS_1,
+                                         "BOOTSTRAP_JS_1": BOOTSTRAP_JS_1
+                                     })
+
+# Tests
+@app.get("/tests")
+async def tests(requests: Request):
+         return template.TemplateResponse("tests.html",
+                                     {
+                                         "request": requests,
+                                         "BOOTSTRAP_CSS_1": BOOTSTRAP_CSS_1,
+                                         "BOOTSTRAP_JS_1": BOOTSTRAP_JS_1
+                                     })
+
+# Run the Application
 if __name__ == "__main__":
     uvicorn.run(app=app, host="127.0.0.1", port=8000)
